7013167360ad73a01608e5d79b256c7a50931c54
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:47:33 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Enemigo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Enemigo
+    {
+        public bool Explode { get; set;}
+        public bool Shoot { get; set; }
+        public bool Movement { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        private int _score;
+        public int Score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                if(value >= 100)
+                {
+                    _score = value;
+                }
+
+                else
+                {
+                    throw new Exception("Score menor a 100");
+                }
+
+            }
+        }
+
+        public void explotar() { }
+        public void moverse() { }
+        public void disparar() { }
+
+
+
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo enemigo = new Enemigo();
+
+            enemigo.Explode = true;
+            enemigo.Movement = true;
+            enemigo.Shoot = true;
+
+            enemigo.PosX = 896;
+            enemigo.PosY = -312;
+
+            Console.WriteLine("Posición en X: " + enemigo.PosX);
+            Console.WriteLine("Posición en Y: " + enemigo.PosY);
+
+
+
+            try
+            {
+                enemigo.Score = 200;
+                Console.WriteLine("Score: " + enemigo.Score);
+            }
+
+            catch(Exception se)
+            {
+                Console.WriteLine("Score menor de 100");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
